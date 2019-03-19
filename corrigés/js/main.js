a086a1b484ea01f1aa7d72ec577ad5e6cc84bc2a
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-03-19 14:56:33 +0100

    Add random color

--- corrigés/js/main.js
+++ corrigés/js/main.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
   let form = document.getElementById("dom-form")
   let wordInput
   let div
+  
 
   //Submit event on form
   form.addEventListener("submit", function(e) {
@@ -14,13 +15,18 @@ document.addEventListener("DOMContentLoaded", function() {
       wordInput = document.getElementById("value").value
       AddText()
   })
+
   
   //Add text to my DOM
   function AddText() {
+
     //Create div
     div = document.createElement('div')
+
     //Add class to my new div
     div.className = 'dynamic-value'
+    div.style.color=getRandomColor()
+    
     //Add text to my new div
     div.innerHTML = wordInput
 
@@ -29,6 +35,26 @@ document.addEventListener("DOMContentLoaded", function() {
 
     //Remove value on my form⁄
     form.reset()
+  }
+
+  
+  //Generate random color
+  function getRandomColor() {
+
+    //Define variable
+    let letters = '0123456789ABCDEF'
+    let color = '#'
+    let i
+
+    //count for get hexa format
+    for (i = 0; i < 6; i++) {
+      
+      //Add the new value
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    //Return the generated color
+    return color;
   }
 
 })
