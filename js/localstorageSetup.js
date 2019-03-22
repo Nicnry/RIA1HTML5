39f6aadcf73ab5e469a01ed7b2495c36917e1a63
Quentin Neves <neves.quentin@gmail.com>
2019-03-22 08:50:32 +0100

    Start js on DOMContentLoaded

--- js/localstorageSetup.js
+++ js/localstorageSetup.js
@@ -1,32 +1,32 @@
-// Setup exercise
-let users = [{ name: "François" }, { name: "Bob" }, { name: "Louis" }]
-let finalUsers = [
-  { name: "François", matched: false },
-  { name: "Bob", matched: false },
-  { name: "Louis", matched: false },
-  { name: "Marcel", matched: false },
-  { name: "Jean-Jacques", matched: false }
-]
-localStorage.setItem("users", JSON.stringify(users))
-JSON.parse(localStorage.getItem("users"))
+document.addEventListener("DOMContentLoaded", function () {
+  // Setup exercise
+  let users = [{ name: "François" }, { name: "Bob" }, { name: "Louis" }]
+  let finalUsers = [
+    ...users,
+    { name: "Marcel", matched: false },
+    { name: "Jean-Jacques", matched: false }
+  ]
+  localStorage.setItem("users", JSON.stringify(users))
 
-document.getElementById("checkButton").addEventListener("click", checkResult)
+  document.getElementById("checkButton").addEventListener("click", checkResult)
 
-function checkResult() {
-  let resultDiv = document.getElementById("result")
-  let result = JSON.parse(localStorage.getItem("users"))
-  resultDiv.innerText = compareResult(finalUsers, result)
-    ? "Correct !"
-    : (resultDiv.innerText = "Incorrect !")
-}
+  function checkResult() {
+    let resultDiv = document.getElementById("result")
+    let result = JSON.parse(localStorage.getItem("users"))
+    resultDiv.innerText = compareResult(finalUsers, result)
+      ? "Correct !"
+      : (resultDiv.innerText = "Incorrect !")
+  }
 
-function compareResult(finalUsers, results) {
-  finalUsers.forEach(finalUser => {
-    results.forEach(result => {
-      if (!finalUser.matched) finalUser.matched = finalUser.name === result.name
+  function compareResult(finalUsers, results) {
+    finalUsers.forEach(finalUser => {
+      results.forEach(result => {
+        if (!finalUser.matched)
+          finalUser.matched = finalUser.name === result.name
+      })
     })
-  })
-  return finalUsers.every(user => {
-    return user.matched
-  })
-}
+    return finalUsers.every(user => {
+      return user.matched
+    })
+  }
+})
