daec8b3cde79f415b87454c57e7f0f9c527978fb
Quentin Neves <neves.quentin@gmail.com>
2019-03-22 10:14:24 +0100

    Fix solution

--- corrigés/js/localstorage.js
+++ corrigés/js/localstorage.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // get currents users
   let myUsers = JSON.parse(localStorage.getItem("users"))
 
-  storageDiv = document.getElementById("storageContent")
-  myUsers.forEach(user => {
-    let userDiv = document.createElement("div")
-    userDiv.innerHTML = user.name
-    storageDiv.appendChild(userDiv)
-  })
+  document.getElementById("displayStorage").addEventListener("click", displayStorage)
 
+  // Display each user in storage
+  function displayStorage() {
+    storageDiv = document.getElementById("storageContent")
+    myUsers.forEach(user => {
+      let userDiv = document.createElement("div")
+      userDiv.innerHTML = user.name
+      storageDiv.appendChild(userDiv)
+    })
+  }
+
+  // Puts back myUsers' content and adds Jean-Jacques & Marcel
   myUsers = [...myUsers, { name: "Jean-Jacques" }, { name: "Marcel" }]
 
+  // Store updated users
   localStorage.setItem("users", JSON.stringify(myUsers))
 })
